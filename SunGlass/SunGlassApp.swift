1737c8ed7c20b8e9ab0e8e21c332df905937cc80
gil <ryogo5.0304@gmail.com>
2026-07-14 09:21:11 +0900

    Initial Commit

---
+++ SunGlass/SunGlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunGlassApp.swift
+//  SunGlass
+//
+//  Created by 落合遼梧 on 2026/07/14.
+//
+
+import SwiftUI
+
+@main
+struct SunGlassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
